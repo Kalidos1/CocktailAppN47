c0fbe2cd1e3e7ec43aee50e38e06eb9c5598f34b
c802011 <shady.eltobgy@sanitas.com>
2019-12-09 10:42:23 +0100

    starting files.

---
+++ src/main/java/com/hackdays/cocktailapp/north47/North47Application.java
@@ -0,0 +1,13 @@
+package com.hackdays.cocktailapp.north47;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class North47Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(North47Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackdays/cocktailapp/north47/North47ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackdays.cocktailapp.north47;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class North47ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
